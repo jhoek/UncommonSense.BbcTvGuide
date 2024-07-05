c0a10ef707cd23ecd593713cf083ff849c5c45ba
Jan Hoek <jhoek@uncommonsense.nl>
2024-07-05 11:58:47 +0200

    Support for multiple dates

--- Public/Get-BbcTvGuide.ps1
+++ Public/Get-BbcTvGuide.ps1
@@ -6,33 +6,38 @@ function Get-BbcTvGuide
         [ValidateSet('bbc-first', 'bbc-one', 'bbc-two', 'bbc-four')]
         [string]$Channel,
 
-        [ValidateNotNullOrEmpty()]
-        [DateTime]$Date = (Get-Date)
+        [ValidateCount(1, [int]::MaxValue)]
+        [DateTime[]]$Date = (0..7 | ForEach-Object { (Get-Date).AddDays($_) })
     )
 
-    $DateString = $Date.ToString("yyyy-MM-dd")
-    $Url = "https://www.bbcbenelux.com/smapi/schedule/nederland/$($Channel)?timezone=Europe%2FLuxembourg&date=$($DateString)"
-
-    Invoke-RestMethod -Uri $Url
-    | ForEach-Object { $_ }
+    $Date
+    | Select-Object -Unique
+    | Sort-Object
     | ForEach-Object {
-        $StartUtc = [DateTime]::SpecifyKind([DateTime]::ParseExact($_.Start, 'yyyy-MM-dd HH:mm:ss', $null), [System.DateTimeKind]::Utc)
-        $EndUtc = [DateTime]::SpecifyKind([DateTime]::ParseExact($_.End, 'yyyy-MM-dd HH:mm:ss', $null), [System.DateTimeKind]::Utc)
+        $DateString = $_.ToString("yyyy-MM-dd")
+        $Url = "https://www.bbcbenelux.com/smapi/schedule/nederland/$($Channel)?timezone=Europe%2FLuxembourg&date=$($DateString)"
 
-        [PSCustomObject]@{
-            PSTypeName      = 'UncommonSense.BccTvGuide.Entry'
-            Channel         = $Channel
-            StartText       = $_.Start
-            EndText         = $_.End
-            StartUtc        = $StartUtc
-            EndUtc          = $EndUtc
-            StartLocal      = $StartUtc.ToLocalTime()
-            EndLocal        = $EndUtc.ToLocalTime()
-            ShowTitle       = $_.Show.Title
-            ShowSynopsis    = $_.Show.Synopsis
-            EpisodeNo       = $_.Episode.Number
-            EpisodeTitle    = $_.Episode.Title
-            EpisodeSynopsis = $_.Episode.Synopsis
+        Invoke-RestMethod -Uri $Url
+        | ForEach-Object { $_ }
+        | ForEach-Object {
+            $StartUtc = [DateTime]::SpecifyKind([DateTime]::ParseExact($_.Start, 'yyyy-MM-dd HH:mm:ss', $null), [System.DateTimeKind]::Utc)
+            $EndUtc = [DateTime]::SpecifyKind([DateTime]::ParseExact($_.End, 'yyyy-MM-dd HH:mm:ss', $null), [System.DateTimeKind]::Utc)
+
+            [PSCustomObject][Ordered]@{
+                PSTypeName      = 'UncommonSense.BccTvGuide.Entry'
+                Channel         = $Channel
+                StartText       = $_.Start
+                EndText         = $_.End
+                StartUtc        = $StartUtc
+                EndUtc          = $EndUtc
+                StartLocal      = $StartUtc.ToLocalTime()
+                EndLocal        = $EndUtc.ToLocalTime()
+                ShowTitle       = $_.Show.Title
+                ShowSynopsis    = $_.Show.Synopsis
+                EpisodeNo       = $_.Episode.Number
+                EpisodeTitle    = $_.Episode.Title
+                EpisodeSynopsis = $_.Episode.Synopsis
+            }
         }
     }
 }
